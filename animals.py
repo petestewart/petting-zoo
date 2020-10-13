af64e4f44122f479f38a2f99af5b9a3ef8f246f1
Pete Stewart <petestew@gmail.com>
2020-10-13 15:46:58 -0500

    intial commit

---
+++ animals.py
@@ -0,0 +1,185 @@
+from datetime import date
+
+class Llama:
+  def __init__(self):
+    self.name = ""
+    self.species = ""
+    self.walking = True
+    self.date_added = date.today()
+
+class Tiger:
+  def __init__(self):
+    self.name = ""
+    self.species = ""
+    self.walking = True
+    self.date_added = date.today()
+
+class Giraffe:
+  def __init__(self):
+    self.name = ""
+    self.species = ""
+    self.walking = True
+    self.date_added = date.today()
+
+class Hippo:
+  def __init__(self):
+    self.name = ""
+    self.species = ""
+    self.walking = True
+    self.date_added = date.today()
+
+class Monkey:
+  def __init__(self):
+    self.name = ""
+    self.species = ""
+    self.walking = True
+    self.date_added = date.today()
+    self.date_added = date.today()
+
+class Dolphin:
+  def __init__(self):
+    self.name = ""
+    self.species = ""
+    self.swimming = True
+    self.date_added = date.today()
+
+class Starfish:
+  def __init__(self):
+    self.name = ""
+    self.species = ""
+    self.swimming = True
+    self.date_added = date.today()
+
+class Eel:
+  def __init__(self):
+    self.name = ""
+    self.species = ""
+    self.swimming = True
+    self.date_added = date.today()
+
+class Octopus:
+  def __init__(self):
+    self.name = ""
+    self.species = ""
+    self.swimming = True
+    self.date_added = date.today()
+
+class Goldfish:
+  def __init__(self):
+    self.name = ""
+    self.species = ""
+    self.swimming = True
+    self.date_added = date.today()
+
+class Copperhead:
+  def __init__(self):
+    self.name = ""
+    self.species = ""
+    self.slithering = True
+    self.date_added = date.today()
+
+class Rat_Snake:
+  def __init__(self):
+    self.name = ""
+    self.species = ""
+    self.slithering = True
+    self.date_added = date.today()
+
+class Salamander:
+  def __init__(self):
+    self.name = ""
+    self.species = ""
+    self.slithering = True
+    self.date_added = date.today()
+
+class Snail:
+  def __init__(self):
+    self.name = ""
+    self.species = ""
+    self.slithering = True
+    self.date_added = date.today()
+
+class Alligator:
+  def __init__(self):
+    self.name = ""
+    self.species = ""
+    self.slithering = True
+    self.date_added = date.today()
+
+
+miss_fuzz = Llama()
+miss_fuzz.name = "Miss Fuzz"
+miss_fuzz.species = "domestic llama"
+
+tony = Tiger()
+tony.name = "Tony"
+tony.species = "hungry tiger"
+
+ned = Giraffe()
+ned.name = "Ned"
+ned.species = "friendly giraffe"
+
+henry = Hippo()
+henry.name = "Henry"
+henry.species = "hippopotamus"
+
+george = Monkey()
+george.name = "George"
+george.species = "chimpanzee"
+
+flipper = Dolphin()
+flipper.name = "Flipper"
+flipper.species = "wild dolphin"
+
+buddha = Starfish()
+buddha.name = "Buddha"
+buddha.species = "golden starfish"
+
+ed = Eel()
+ed.name = "Ed"
+ed.species = "electric eel"
+
+gus = Octopus()
+gus.name = "Gus"
+gus.species = "wild octopus"
+ed.species = "electric eel"
+
+jerry = Goldfish()
+jerry.name = "Jerry"
+jerry.species = "wild octopus"
+
+carl = Copperhead()
+carl.name = "Carl"
+carl.species = "copperhead snake"
+
+ren = Rat_Snake()
+ren.name = "Ren"
+ren.species = "poisonous rattler"
+
+sally = Salamander()
+sally.name = "Sally"
+sally.species = "domestic salamander"
+
+charlie = Snail()
+charlie.name = "Charile"
+charlie.species = "domestic snail"
+
+al = Alligator()
+al.name = "Al"
+al.species = "dangerous gator"
+
+print(miss_fuzz)
+print(tony)
+print(ned)
+print(henry)
+print(george)
+print(flipper)
+print(buddha)
+print(ed)
+print(gus)
+print(jerry)
+print(carl)
+print(ren)
+print(sally)
+print(charlie)
+print(al)
