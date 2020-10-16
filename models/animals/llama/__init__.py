0a563d3810cd5e65e799404b7b1fed89eb40e06f
Pete Stewart <petestew@gmail.com>
2020-10-16 13:22:31 -0500

    composition types created and modularized

---
+++ models/animals/llama/__init__.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+class Llama:
+  def __init__(self, name, species, food):
+    self.name = name
+    self.species = species
+    self.shift = ""
+    self.walking = True
+    self.date_added = date.today()
+    self.food = food
+
+  def feed(self):
+    print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+
+  def __str__(self):
+    return f"{self.name} is a {self.species}"
