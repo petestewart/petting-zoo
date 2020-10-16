8b052f6ffb36f8d89c5dcaf17d639a75e7b9ffb4
Pete Stewart <petestew@gmail.com>
2020-10-16 14:36:28 -0500

    getters and setters created

--- models/attractions/wetlands/__init__.py
+++ models/attractions/wetlands/__init__.py
@@ -12,3 +12,7 @@ class Wetlands:
       attraction_animals.append(f" * {animal.name}")
     animals_list = ('\n'.join(map(str, attraction_animals)))
     return f"{self.attraction_name} is where you'll find {self.description} like \n{animals_list}"
+
+  @property
+  def last_critter_added(self):
+    return f"{self.animals[-1].name} the {self.animals[-1].species}" 
